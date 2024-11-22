a75a168588cd18febc9f1213b66464e6d26c4b16
Aquib Ahmed <aquibahmed21@gmail.com>
2024-11-22 20:18:01 +0530

    Updates

--- typescript/home.ts
+++ typescript/home.ts
@@ -2,7 +2,7 @@ export function setupHome(element: HTMLDivElement) {
   let counter = 0
   const setCounter = (count: number) => {
     counter = count
-		element.innerHTML = "Aquib Ahmed" + ": " +counter;
+		element.innerHTML = "Aquib Ahmed " + ": " +counter;
   }
   element.addEventListener('click', () => setCounter(counter + 1))
   setCounter(0)
